e5edda22faa9f8b2554a1e76ca8cd05fb5ab5898
Vishwajeet Salunke <salunkevishwajeet577@gmail.com>
2023-04-18 14:56:14 +0000

    18/04/23

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
-const express = require('express');
-const https = require('https');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const https = require("https");
 
-app.get('/',function(req,res){
-    res.send("hello world i'm vishwajeet");
+// this is a folder for including static files in website
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname+"/signup.html")
 });
 
-app.listen(3000,function(){
-    console.log("server started at port 3000");
+app.post("/",function(req,res){
+    var fName = req.body.firstName;
+    var lName = req.body.lastName;
+    var email = req.body.email;
+    res.send("information recieved");
+    // var message = {
+    //   members:[
+    //     {
+    //       email_address : email,
+    //       status: "subscribed",
+    //       merge_fields:{
+    //         FNAME: fName,
+    //         LNAME:lName
+    //       }
+    //     }
+    //   ]
+    // };
+
+    console.log(req.body);
+
+
+});
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
 });
